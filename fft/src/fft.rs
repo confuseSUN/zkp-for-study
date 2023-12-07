2effe8e2ddb759b4103c6ea457357c88a23139cc
confuseSUN <778398626@qq.com>
2023-12-07 17:09:01 +0800

    Add comparison test

--- fft/src/fft.rs
+++ fft/src/fft.rs
@@ -13,20 +13,20 @@ pub fn radix2_fft<F: PrimeField>(coeffs: &[F], domain: &[F]) -> Vec<F> {
     let mut stage = 0;
 
     while (1 << stage) < n {
-        let num_domain_elements_needed = 1 << stage;
-        let step = num_domain_elements_needed * 2;
+        let required_domain_elements = 1 << stage;
+        let step = required_domain_elements * 2;
 
-        for i in 0..num_domain_elements_needed {
+        for i in 0..required_domain_elements {
             let w = domain[i * (n / step)];
 
             for j in (i..n).step_by(step) {
-                let w_j = w * coeffs[j + num_domain_elements_needed];
+                let w_j = w * coeffs[j + required_domain_elements];
 
                 let r_0 = coeffs[j] + w_j;
                 let r_1 = coeffs[j] - w_j;
 
                 coeffs[j] = r_0;
-                coeffs[j + num_domain_elements_needed] = r_1;
+                coeffs[j + required_domain_elements] = r_1;
             }
         }
 
--- fft/src/test.rs
+++ fft/src/test.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use ark_bls12_381::Fr;
 use ark_ff::PrimeField;
 use ark_poly::{
@@ -29,6 +31,25 @@ fn test_fft() {
     let values = radix2_fft(&coeffs, &domain);
 
     let expected = native_eval(&coeffs);
+
+    assert_eq!(values, expected);
+}
+
+#[test]
+fn test_comparison() {
+    let coeffs = vec![Fr::from(100); 1048576];
+    let domain = evaluation_domain(coeffs.len());
+
+    /*   Custom Radix-2 FFT  */
+    let start = Instant::now();
+    let values = radix2_fft(&coeffs, &domain);
+    println!("radix2 fft : {:02?}", start.elapsed());
+
+    /*   Arkwork‘s FFT  */
+    let domain = Radix2EvaluationDomain::<Fr>::new(coeffs.len()).unwrap();
+    let start = Instant::now();
+    let expected = domain.fft(&coeffs);
+    println!("arkwork's fft : {:02?}", start.elapsed());
 
     assert_eq!(values, expected);
 }
