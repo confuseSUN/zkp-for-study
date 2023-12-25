d03857649915ccbdb3d6060ee271b954e2646e08
confuseSUN <778398626@qq.com>
2023-12-25 09:30:54 +0800

    comments

--- stark_by_hand/src/main.rs
+++ stark_by_hand/src/main.rs
@@ -18,6 +18,8 @@ pub mod utils;
 fn main() {
     let public_inputs = vec![Fr::from(24), Fr::from(30), Fr::from(28)];
 
+    // 1. The Execution Trace with padding.
+
     let data_column_1 = vec![24, 30, 54, 84, 78, 15, 29, 50];
     let data_column_2 = vec![30, 54, 84, 41, 2, 77, 21, 36];
     let data_column_3 = vec![54, 84, 41, 28, 71, 17, 92, 33];
@@ -35,32 +37,40 @@ fn main() {
         control_column_3.iter().map(|x| Fr::from(*x)).collect(),
     ];
 
+    // 2. Constructing Trace Polynomials
+
     // println!("The six trace polynomials:");
     let mut trace_polys = Vec::new();
     let domain = Radix2EvaluationDomain::<Fr>::new(trace[0].len()).unwrap();
     for t in trace.iter() {
         let coeffs = domain.ifft(t);
-        //  println!("{:?}", coeffs);
+        // println!("{:?}", coeffs);
         trace_polys.push(coeffs);
     }
+
+    // 3. Evaluating the trace polynomial on the larger domain gives a Reed-Solomon encoded trace block.
 
     //  println!("\n The reedsolomon codes of trace polynomial:");
     let mut trace_reedsolomon_codes = Vec::new();
     let expanded_domain = Radix2EvaluationDomain::<Fr>::new(trace[0].len() * 4).unwrap();
     for coeffs in trace_polys.iter() {
         let evals = expanded_domain.fft(coeffs);
-        //   println!("{:?}", evals);
+        // println!("{:?} \n", evals);
         trace_reedsolomon_codes.push(evals);
     }
+
+    // 4. ZK Commitments of the Trace Data.
 
     //  println!("\n The zk commitments of trace polynomial:");
     let mut trace_zk_commitments = Vec::new();
     let expanded_coset_domain = expanded_domain.get_coset(Fr::GENERATOR).unwrap();
     for coeffs in trace_polys.iter() {
         let evals = expanded_coset_domain.fft(coeffs);
-        // println!("{:?}", evals);
+        // println!("{:?} \n", evals);
         trace_zk_commitments.push(evals);
     }
+
+    // 5. Constraint Polynomials.
 
     let constraints = FibonacciConstraint::construct(&trace, &public_inputs, Label::Trace);
     let constraint_reedsolomon_codes = FibonacciConstraint::construct(
@@ -77,28 +87,35 @@ fn main() {
     // println!("constraint reedsolomon codes:\n{:?}", constraint_reedsolomon_codes);
     // println!("constraint zk commitments:\n{:?}", constraint_zk_commitments);
 
-    let mix_alpha = Fr::from(3);
-    let mixed_constraint_column = constraints.mix(&mix_alpha);
-    let mixed_constraint_reedsolomon_code = constraint_reedsolomon_codes.mix(&mix_alpha);
-    let mixed_constraint_zk_commitment = constraint_zk_commitments.mix(&mix_alpha);
+    // 6. Mixing Constraint Polynomials.
 
-    let constraint_polys = constraint_reedsolomon_codes.ifft(&expanded_domain);
-    let mixed_constraint_polys = expanded_domain.ifft(&mixed_constraint_reedsolomon_code);
+    let mix_alpha = Fr::from(3);
+    let _mixed_constraint_column = constraints.mix(&mix_alpha);
+    let _mixed_constraint_reedsolomon_code = constraint_reedsolomon_codes.mix(&mix_alpha);
+    let mixed_constraint_zk_commitment = constraint_zk_commitments.mix(&mix_alpha);
+    // println!("mixed constraint :{:?} \n", mixed_constraint_zk_commitment);
+
+    // let _constraint_polys = constraint_reedsolomon_codes.ifft(&expanded_domain);
+    // let _mixed_constraint_polys = expanded_domain.ifft(&mixed_constraint_reedsolomon_code);
     // println!("constraint polys:\n{:?}",constraint_polys);
-    // println!("mixed constraint:{:?} \n", mixed_constraint_polys);
+    // println!("mixed constraint polys:{:?} \n", mixed_constraint_polys);
+
+    //  7. Constructing the validity polynomial by dividing the mixed constraint polynomial by the zeros polynomial.
 
     let mut z_x = vec![Fr::ZERO; 9]; // domain.vanishing_polynomial();
     z_x[0] = Fr::ONE.neg();
     z_x[8] = Fr::ONE;
-    let z_x_reedsolomon_code = expanded_domain.fft(&z_x);
+    let _z_x_reedsolomon_code = expanded_domain.fft(&z_x);
     let z_x_zk_commitment = expanded_coset_domain.fft(&z_x);
     // println!("reedsolomon code of z_x:{:?}", z_x_reedsolomon_code);
     // println!("zk commitment of z_x:{:?}", z_x_zk_commitment);
 
     let validity_poly_evals = vec_div(&mixed_constraint_zk_commitment, &z_x_zk_commitment);
     let validity_poly_coeffs = expanded_coset_domain.ifft(&validity_poly_evals);
-    // println!("validity_poly_evals:{:?} ", validity_poly_evals);
+    // println!("validity_poly_evals:{:?}", validity_poly_evals);
     // println!("validity_poly_coeffs:{:?} ", validity_poly_coeffs);
+
+    // 8. The DEEP Technique.
 
     let deep_test_point = Fr::from(93);
     let deep_test_point_forward = deep_test_point.mul(&domain.group_gen_inv());
@@ -166,7 +183,9 @@ fn main() {
         &expanded_coset_domain,
     );
 
-    let fri_input = mix(
+    // 9. Mixing for FRI.
+
+    let fri_input_poly_evals = mix(
         &[
             deep_poly1_evals,
             deep_poly2_evals,
@@ -179,5 +198,8 @@ fn main() {
         &Fr::from(21),
     );
 
-    println!("{:?}", expanded_domain.ifft(&fri_input));
+    println!(
+        "fri_input_poly_evals: {:?}",
+        expanded_domain.ifft(&fri_input_poly_evals)
+    );
 }
